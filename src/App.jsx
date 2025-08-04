437f49a9137ce3665b99745aaeeca1415fc2f09e
xaker1020 <qodirjonovhabibulloh8@gmail.com>
2025-08-04 15:07:11 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <h1>welcome TO react JS</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis reiciendis est quisquam provident porro quas in quam minus ut nisi.</p>
+<ul>
+  <li>HTLMM</li>
+  <li>CSS</li>
+  <li>Javascript</li>
+  <li>React</li>
+</ul>
+<h1>What is React</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed sint maiores error omnis debitis vel exercitationem voluptate, ad accusantium dolorem ab quos, cum magnam nostrum sit maxime officiis a! Officiis voluptatem consectetur quibusdam optio quae soluta? Dolorem laborum aspernatur voluptate quos fuga maiores! Voluptatem perspiciatis voluptates explicabo praesentium sint deleniti?</p>
+<h1>What is Single-page application(SPA)</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laborum nesciunt fugiat. Aut consequuntur quasi quisquam nulla! Error facere, doloribus, animi veritatis voluptatem quia ipsam in at perspiciatis nemo fugiat aliquam dicta quae, distinctio voluptatum magnam temporibus tempore facilis! Libero suscipit itaque labore facere maxime distinctio nihil voluptas fuga minima.`</p> 
+   </div>
+  )
+}
+
+export default App
